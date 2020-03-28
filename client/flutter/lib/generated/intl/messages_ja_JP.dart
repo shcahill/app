517e19126c3d5f9064e389fb4aeb3dfa5711ef8a
kikuchy <kikuchy.h@gmail.com>
2020-03-28 20:46:45 +0900

    :sparkles: feat(i18n): add ja_JP translation
    Add Japanese translation

---
+++ client/flutter/lib/generated/intl/messages_ja_JP.dart
@@ -0,0 +1,73 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ja_JP locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ja_JP';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "commonWorldHealthOrganization" : MessageLookupByLibrary.simpleMessage("世界保健機関"),
+    "healthCheckTitle" : MessageLookupByLibrary.simpleMessage("ヘルスチェック"),
+    "homePagePageButtonLatestNumbers" : MessageLookupByLibrary.simpleMessage("最新の数値"),
+    "homePagePageButtonProtectYourself" : MessageLookupByLibrary.simpleMessage("自身の身を守るために"),
+    "homePagePageButtonTravelAdvice" : MessageLookupByLibrary.simpleMessage("お出かけの際のアドバイス"),
+    "homePagePageButtonWHOMythBusters" : MessageLookupByLibrary.simpleMessage("デマに惑わされないために"),
+    "homePagePageButtonYourQuestionsAnswered" : MessageLookupByLibrary.simpleMessage("疑問と回答"),
+    "homePagePageSliverListAboutTheApp" : MessageLookupByLibrary.simpleMessage("このアプリについて"),
+    "homePagePageSliverListAboutTheAppDialog" : MessageLookupByLibrary.simpleMessage("世界保健機関 (WHO) 公式の COVID-19 関連情報をお伝えするアプリです。"),
+    "homePagePageSliverListProvideFeedback" : MessageLookupByLibrary.simpleMessage("このアプリに関するフィードバック"),
+    "homePagePageSliverListShareTheApp" : MessageLookupByLibrary.simpleMessage("このアプリをシェアする"),
+    "protectYourselfListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("石鹸と流水で、こまめに手を洗ってください"),
+    "protectYourselfListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("目、口、鼻を触らないようにしましょう"),
+    "protectYourselfListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("咳やくしゃみをするときは、曲げた肘やティッシュで口や鼻を覆いましょう"),
+    "protectYourselfListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("人混みを避けましょう"),
+    "protectYourselfListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("たとえ微熱や軽いせきであっても、具合が悪いときは家の中で過ごすようにしてください"),
+    "protectYourselfListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("発熱、咳、呼吸困難などの症状が現れた場合は、早めに医療機関を受診してください。受診の前に、必ず電話で連絡をとってください！"),
+    "protectYourselfListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("WHOからの最新情報を受け取れるようにしておいてください"),
+    "travelAdviceListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("WHO はCOVID-19が蔓延している国に対しての渡航や貿易の制限をかけないように各国へアドバイスしています…"),
+    "travelAdviceListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("COVID-19 の症状がある人、またはその疑いがある人の世話をするときだけマスクをしましょう"),
+    "travelAdviceListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("感染地域から帰国される方は:"),
+    "travelAdviceListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("ここ14日間の症状を自分で顧み、入国しようとしている国の規則に従ってください。国によっては、帰国者に検疫所への立ち入りを要求する場合があります"),
+    "travelAdviceListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("サーモグラフィーでは、熱があるか否か、はわかりますが、コロナウィルスに感染しているか否か、はわかりません"),
+    "travelAdviceListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("もし、熱や咳、呼吸困難などの症状が現れた場合は、できれば電話で、現地の医療機関と連絡をとって、症状と渡航歴を伝えてください"),
+    "travelAdviceListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("病気を患っている旅行者は、特に高齢の旅行者や慢性疾患や基礎疾患を持つ人々のために、被災地への旅行を遅らせたり、避けたりすることが賢明です…"),
+    "travelAdviceListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("“感染地域”とは、国外から持ち込まれた症例のみが報告されている地域とは対照的に、 COVID-19 の感染が継続している国、州、地域、都市を指します。“Affected areas” are considered those countries, provinces, territories or cities experiencing ongoing transmission of COVID-19, in contrast to areas reporting only imported cases…"),
+    "travelAdviceListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("すべての旅行者への一般的なお勧めは、以下のとおりです。"),
+    "travelAdviceListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("石鹸と流水で、こまめに手を洗ってください"),
+    "travelAdviceListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("目、口、鼻を触らないようにしましょう"),
+    "travelAdviceListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("咳やくしゃみをするときは、曲げた肘やティッシュで口や鼻を覆いましょう"),
+    "travelAdviceListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("具合の悪い人から1メートル以上離れましょう"),
+    "travelAdviceListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("適切に栄養を取りましょう"),
+    "whoMythBustersListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("沢山の虚偽情報が出回っています。判明している真実は以下のとおりです。"),
+    "whoMythBustersListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("サーモグラフィーでは、熱があるか否か、はわかりますが、コロナウィルスに感染しているか否か、はわかりません"),
+    "whoMythBustersListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("アルコールや塩素を全身にスプレーしても、すでに体内に入ってしまったコロナウィルスは殺せません"),
+    "whoMythBustersListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("肺炎球菌ワクチンやヘモフィルスインフルエンザ菌b型（Hib）ワクチンなどの肺炎に対するワクチンは、コロナウィルスに対しては効果がありません"),
+    "whoMythBustersListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("食塩水での定期的な鼻うがいがコロナウィルスの感染を防ぐ、というエビデンスはありません"),
+    "whoMythBustersListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("ニンニクは健康に良いですが、ニンニクを食べることで現在のコロナウィルスの流行から人を守ることが出来た、というエビデンスはありません"),
+    "whoMythBustersListOfItemsPageListItem15" : MessageLookupByLibrary.simpleMessage("抗生物質はウィルスには効果がありません。抗生物質は効果を発揮するのはバクテリアに対してです"),
+    "whoMythBustersListOfItemsPageListItem16" : MessageLookupByLibrary.simpleMessage("今のところ、コロナウィルスの予防や治療に対して推奨される薬はありません"),
+    "whoMythBustersListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("すべての年齢層の人が感染する可能性があります。高齢者、基礎疾患を持っている方（喘息、糖尿病、心臓病など）は重症化しやすい傾向にあります"),
+    "whoMythBustersListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("低気温や雪ではコロナウィルスは死滅しません"),
+    "whoMythBustersListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("コロナウィルスは高温多湿な地域でも伝染する可能性があります"),
+    "whoMythBustersListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("コロナウィルスは蚊によって媒介されるものではありません"),
+    "whoMythBustersListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("犬や猫といったコンパニオンアニマル/ペットがコロナウィルスを媒介するというエビデンスはありません"),
+    "whoMythBustersListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("熱いお湯に入浴することではコロナウィルスは防げません"),
+    "whoMythBustersListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("ハンドドライヤーにはコロナウィルスを殺す効果はありません"),
+    "whoMythBustersListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("紫外線は殺菌消毒のために使うべきではありません。逆に皮膚のかぶれを引き起こしかねません")
+  };
+}
